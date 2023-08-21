17edfef6541767794233f57dd58e2d39acba2286
Div-Inworld <divakar@inworld.ai>
2023-08-21 17:11:37 -0400

    Firrst stage of protobuf removal with replacement data structures

---
+++ Assets/Inworld/Inworld.NDK/Scripts/InworldNDKPackets.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace Inworld.NDK
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimpleActor {
+        public int type;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string name;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimpleRouting {
+        public SimpleActor source;
+        public SimpleActor target;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimplePacketId {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string packet_id;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string utterance_id;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string interaction_id;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string correlation_id;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimpleInworldPacket {
+        public long timestamp_seconds;
+        public int timestamp_nanos;
+        // ... Add other members ...
+
+        public PacketUnion packet;
+        public int packetType;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PacketUnion {
+        [FieldOffset(0)] public SimpleTextEvent textEvent;
+        // ... other events will be added here ...
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimpleTextEvent {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+        public string text;
+        public int sourceType;
+        public bool isFinal;
+    }
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SimpleControlEvent {
+        public int action;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+        public string description;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+        public string payload;
+    }
+}
